965fe5b37c7180d86acc70da56efc33e92ef6386
Yassin-567 <83673138+Yassin-567@users.noreply.github.com>
2025-05-13 00:23:04 +0300

    Production 13/5 12:22am

--- stock/settings.py
+++ stock/settings.py
@@ -85,19 +85,18 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #Database
 #https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+#DATABASES = {
+#    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
-# from decouple import config
-# import dj_database_url
+from decouple import config
+import dj_database_url
 
-# DATABASES = {
-#     'default': dj_database_url.parse(config('DATABASE_URL'))
-# }
+DATABASES = {
+    'default': dj_database_url.parse(config('DATABASE_URL')) }
 
 
 
