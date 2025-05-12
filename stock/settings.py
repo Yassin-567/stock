93b2b7bd39573a831a0cdf77f7af844fcda6346d
Yassin-567 <83673138+Yassin-567@users.noreply.github.com>
2025-05-13 00:42:47 +0300

    Production 13/5 12:42am

--- stock/settings.py
+++ stock/settings.py
@@ -87,7 +87,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #DATABASES = {
 #    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+#       'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
